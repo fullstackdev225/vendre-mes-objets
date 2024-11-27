e77fbaf769ab5877840b66561232b62e6399021c
Potey Charles <ppsjcfullstackdeveloper@gmail.com>
2024-11-27 12:09:03 +0000

    Réalisation de la page d'accueil, d'inscription et de connexion côté front-end

---
+++ templates/homepage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta name="description" content="L'application vendre mes objets est une application web qui permet auix utilisateurs de vendre leurs articles en ligne">
+         <title>VendreMesObjets - L'application de vente d'objets en ligne</title>
+         <link rel="stylesheet" href="css/main.css">
+         <link rel="shortcut icon" href="icons/vendre-mes-objets.png" type="image/x-icon">
+    </head>
+
+    <body>
+        <div class="container-fluid">
+           <div class="row">
+               <div class="col-md-6">
+                    <div class="text-center">
+                        <img src="icons/vendre-mes-objets.png" width="300" alt="app's icon">
+                    </div>
+
+                    <h1 class="text-center">L'application pour vendre vos objets en ligne</h1><br>
+
+                    <p class="text-secondary" style="text-align: justify;">
+                        L'application <span class="fw-bold">VendreMesObjets</span> est une application intuitive et
+                        conviviale permettant aux utilisateurs d'acheter et de vendre facilement des articles
+                        neufs ou d'occasion. Elle offre une expérience fluide grâce à une interface moderne,
+                        des outils de recherche avancée, et des fonctionnalités sécurisées pour
+                        les transactions. Que ce soit pour dénicher une bonne affaire ou donner
+                        une seconde vie à vos objets, l'application facilite les échanges tout
+                        en favorisant une consommation responsable et connectée.
+                    </p><br>
+
+                    <div class="text-center">
+                        <a href="src/controllers/signup.php" class="btn btn-primary">Inscription</a>
+                        <a href="src/controllers/login.php" class="btn btn-info text-light">Connexion</a>
+                    </div>
+               </div>
+
+               <div class="col-md-6">
+                   <img src="images/img-home.jpg" alt="" class="img-fluid ms-3">
+               </div>
+           </div>
+        </div>
+    </body>
+</html>
